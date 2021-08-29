9105c4f3d200957b1770043cd32cc73194528a80
XuHuang <837851874@qq.com>
2021-08-29 22:58:56 +0400

    Time: 56 ms (88.54%), Space: 15.5 MB (14.26%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        暴力思路：每个数字和后面的数字加起来的和如果等于target，便返回下标。
+        一种O(n)复杂度的思路，如果想知道2和谁相加等于9，先存储2的下标和（9-2）的值，当nums[i]等于（9-2）时，返回2的下标和当前i的值
+        参考：https://leetcode.com/problems/two-sum/discuss/17/Here-is-a-Python-solution-in-O(n)-time
+        用一个字典存储需求的值和当前下标，遍历一遍，当需求的值出现时返回之前的下标与当前需求值的下标。
         """
-        for idx, i in enumerate(nums[0:len(nums)-1]):
-            for idx2,j in enumerate(nums[idx+1:len(nums)]):
-                if i+j == target:
-                    return [idx,idx+idx2+1]
+        buffer_dic = {}
+
+        for i in range(nums.__len__()):
+            if nums[i] not in buffer_dic:
+                buffer_dic[target-nums[i]] = i
+            else:
+                return [buffer_dic[nums[i]],i]
+        
+        
