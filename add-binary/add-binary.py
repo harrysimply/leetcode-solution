3ede309139d83926f2fcecee2f2f358643196473
XuHuang <837851874@qq.com>
2021-08-30 23:13:57 +0400

    Time: 41 ms (14.34%), Space: 14.3 MB (22.41%) - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,5 +1,9 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        """
+        在面试时肯定会被干掉的解法： 
+        """
+        
         int_a = int(a, 2)
         int_b = int(b, 2)
         return str(bin(int_a + int_b))[2:]
