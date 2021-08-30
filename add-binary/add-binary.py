e3f0efe4ddaaf1c71b43a519c1326e9b6157bed6
XuHuang <837851874@qq.com>
2021-08-31 01:06:35 +0400

    Time: 44 ms (13.90%), Space: 14.3 MB (22.55%) - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,10 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         """
-        在面试时肯定会被干掉的解法： 
-        """
+        两个二进制字符串，相加后得到新的二进制字符串。
+        下一步就需要考虑进位carry
+        从两个字符串最右边列开始计算，如果大于2则进一位
         
-        int_a = int(a, 2)
-        int_b = int(b, 2)
-        return str(bin(int_a + int_b))[2:]
+        """
+        i, j, summ, curry = len(a)-1, len(b)-1, "", 0
+        
+        while i>=0 or j>=0 or curry!=0:
+            d1 = int(a[i]) if i >= 0 else 0 # 补齐二进制字符串长度较短空位
+            d2 = int(b[j]) if j >= 0 else 0  
+            summ += str((d1+d2+curry)%2)
+            curry = (d1+d2+curry)//2
+            i,j = i-1,j-1
+        return summ[::-1]
         
