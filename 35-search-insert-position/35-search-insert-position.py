3c702fdad2d3a33a3c1ddbdab34fd35dec38b48d
XuHuang <837851874@qq.com>
2022-01-07 01:07:24 +0400

    Time: 52 ms (53.34%), Space: 14.9 MB (94.45%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -3,14 +3,7 @@ class Solution:
         '''
         理解下题意，在一个有序列表中找到target的位置，如果没找到就返回target在列表中的位置。必须使用O(logn)的时间复杂度。
         '''
-        if target in nums:
-            return nums.index(target)
-        else:
-            if target < nums[0]:
-                return 0
-            elif target > nums[-1]:
-                return len(nums)
-            else:
-                for idx,i in enumerate(nums[1:]):
-                    if i> target:
-                        return idx+1
+        for idx,i in enumerate(nums):
+            if i >= target:
+                return idx
+        return len(nums)    
