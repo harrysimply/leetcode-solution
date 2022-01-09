d4cd769e18d111d9bbb69ce6863ac40e6cb2d571
XuHuang <837851874@qq.com>
2022-01-09 15:41:04 +0400

    Time: 79 ms (32.60%), Space: 14.5 MB (11.65%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,26 +10,26 @@ class Solution:
         1. 两个单链表中的数字相加小于9，新的结果链表的值为两数之和
         2. 两个单链表中的数字相加大于等于10，新的结果连表值为两数之和的各位，并且链表的下一位加1.
         
-        如果两个相加的链表有一个链表加完了，则新链表的next可以指向没加完的链表。
+        
+        参考： https://www.youtube.com/watch?v=aM4Iv7eEr2o
         """
         dummy = head = ListNode(0)
         carry = 0
         
+        # 两数相加，直到链表为空
         while l1 != None or l2 != None:   
-            if l1 == None:
-                l1_val = 0
-            else:
-                l1_val = l1.val
-            if l2 == None:
-                l2_val = 0
-            else:
-                l2_val = l2.val
+            # 当其中一个链表为空的时候赋值为0
+            l1_val = 0 if l1 == None else l1.val
+            l2_val = 0 if l2 == None else l2.val
+
             sum_res = 0    
             current_sum = l1_val + l2_val + carry
             carry = current_sum // 10
             new_kept_num = current_sum % 10
+            # 将两数相加和与10取余的结果保存到新的链表中
             dummy.next = ListNode(new_kept_num)
 
+            #如果链表不为空，则移动链表
             if l1 !=None: l1 = l1.next
             if l2 !=None: l2 = l2.next
             dummy = dummy.next
