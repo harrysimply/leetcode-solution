6b5afc702dfdb4fa19c150d9d0dfd1205e272bce
XuHuang <837851874@qq.com>
2022-01-10 19:28:12 +0400

    Time: 64 ms (62.95%), Space: 14.2 MB (50.43%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -2,9 +2,26 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         '''
         回文数字。如果一个数前后都相等则称为回文数，如果是负数则不是回文数
+        # if x < 0 or str(x) !=str(x)[::-1]:
+        #     return False
+        # else:
+        #     return True
+        
+        新方法： x//10 得到 前面的数字， x%10得到后面的数字，通过一个循环获得一个新的数字
+        
         '''
-        if x < 0 or str(x) !=str(x)[::-1]:
+        if x < 0:
             return False
-        else:
-            return True
+        b = x
+        res = 0
+        while x > 0:
+            res = 10 * res + x % 10
+            
+            x = x // 10
+            
+        return res == b
+            
+        
+        
+
         
