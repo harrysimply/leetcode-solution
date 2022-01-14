527dd4790bffd9f95d66402897bad41017ab9541
XuHuang <837851874@qq.com>
2022-01-14 17:39:31 +0400

    Time: 46 ms (47.25%), Space: 14.4 MB (7.24%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -4,8 +4,7 @@ class Solution:
         给一个整数，计算他的开方数是多少
         x = res * res 计算res
         从0开始遍历res，如果res^2 > x 返回res
-        '''
-        res = 0
+                res = 0
         
         while res*res <= x:
             
@@ -14,3 +13,20 @@ class Solution:
             res +=1
             
         return res-1
+        空间O(1),时间O(n)
+        也可以使用二分查找，空间复杂度O(1)时间复杂度降低到O(logn)
+        '''
+        l,r = 1, x
+        if x < 2:
+            return x
+        while(l < r):
+            mid = l + (r-l) // 2
+            if mid * mid  == x:
+                return mid
+            elif mid * mid > x:
+                r = mid
+            elif mid * mid < x:
+                l = mid + 1 # 跳出循环的关键
+        return l-1
+        
+        
