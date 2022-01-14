d23a539d3a3bd8c17a659188379da4e4bef4a25c
XuHuang <837851874@qq.com>
2022-01-14 18:21:52 +0400

    Time: 43 ms (25.34%), Space: 14.3 MB (52.10%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,42 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        '''
+        计算x的n次指数是多少？
+        当n是正整数时，得到的是正整数；当x是负指数时，得到的是小数
+        使用暴力解法会超时：
+        res = 1.0
+        if n > 0:
+            
+            for i in range(n):
+                res*=x
+            return res 
+        elif n == 0:
+            return res
+        else:
+            new_n = abs(n)
+            
+            for i in range(new_n):
+                res *=x
+            return 1 / res
+                
+        使用二分法，使
+        x^10 = x^5 * x^5
+        x^ 5 = x * x^2 * x^ 2
+        x^2 = x^1* x^1
+        x^1 = x * x^0 * x^0
+        '''
+        
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+            tmp = helper(x, n//2)
+            return tmp*tmp if n%2 ==0 else x*tmp*tmp
+        
+        
+        res = helper(x, abs(n))
+        
+        return res if n > 0 else 1/res
+        
+        
+
+        
