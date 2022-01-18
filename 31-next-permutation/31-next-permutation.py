b6fa1620ffd850b2ec9bc21f20112e60c5ef0e45
XuHuang <837851874@qq.com>
2022-01-18 23:59:42 +0400

    Time: 57 ms (31.64%), Space: 14.3 MB (20.35%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         https://www.youtube.com/watch?v=9Xxv6J88KVs
         看了题目开始看不懂这个题到底想让我们干什么。。。后来看了视频才了解到是一组数字，找出来这组数组的排列组合中，比当前数字的大的数字组合中最小的那一个。如果整个数组都是逆序排列的，则返回最小升序排列数组。
-        
+        题解参考：https://www.youtube.com/watch?v=IbcQOdtmvpA
         """
         def reverse(nums, i , j):
             while i<j:
