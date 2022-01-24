21e1b7adf718c6ef6c9b64a07c887445d371fd5a
XuHuang <837851874@qq.com>
2022-01-24 22:38:37 +0400

    Time: 130 ms (37.95%), Space: 14.5 MB (14.19%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+        给出一个二位列表，验证所有已填入的数值满足数独的规则。
+        返回true或者false
+        参考：https://leetcode.com/problems/valid-sudoku/discuss/15451/A-readable-Python-solution
+        """
+        return (self.is_row_valid(board) and
+                self.is_col_valid(board) and
+                self.is_square_valid(board))
+    
+    def is_unit_valid(self, unit):
+        unit = [i for i in unit if i != '.']
+        return len(set(unit)) == len(unit)
+    
+    def is_row_valid(self, board):
+        for row in board:
+            if not self.is_unit_valid(row):
+                return False
+        return True
+    
+    def is_col_valid(self, board):
+        for col in zip(*board):
+            if not self.is_unit_valid(col):
+                return False
+        return True
+    
+    def is_square_valid(self, board):
+        for i in (0,3,6):
+            for j in (0,3,6):
+                square = [ board[x][y] for x in range(i,i+3) for y in range(j, j+3)]
+                if not self.is_unit_valid(square):
+                    return False
+        return True
