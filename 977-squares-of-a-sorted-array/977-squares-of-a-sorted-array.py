2d409d6f95a7584052532c61b5048805fc66d8b2
Huang Xu <837851874@qq.com>
2022-01-27 22:12:17 +0400

    Time: 427 ms (15.22%), Space: 16.3 MB (12.69%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -7,15 +7,16 @@ class Solution:
         题目说要使用O(n)的时间复杂度，因此考虑使用双指针的方法
         '''
         l, r = 0, len(nums)-1
-        res = []
-        for i in range(len(nums)):
+        res = [0]*len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            # 如果左边的数字的绝对值比右边的大
             if abs(nums[l]) > abs(nums[r]):
-                res.append(nums[l]*nums[l])
+                res[i]=nums[l]*nums[l]
                 l+=1
             else:
-                res.append(nums[r]*nums[r])
+                res[i]=nums[r]*nums[r]
                 r-=1
-        return res[::-1]
+        return res
                 
                 
         
