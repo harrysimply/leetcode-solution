37a520c6452799e1fc04190aab9c387bcb966220
Huang Xu <837851874@qq.com>
2022-01-28 22:39:38 +0400

    Time: 328 ms (69.12%), Space: 28.8 MB (59.11%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+    '''
+    设计一个数据结构（字典），可以给里面加新词也可以去搜索是否匹配上。
+    并且还包括通配符，如果有“.”就可以匹配任意字符。
+    使用字典数数据结构
+    '''
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def addWord(self, word: str) -> None:
+        
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(j,root):
+            cur = root
+            for i in range(j, len(word)):
+                c= word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
