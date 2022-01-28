0ef2158d41b222d7400bc7aa4df0010ee36927ad
Huang Xu <837851874@qq.com>
2022-01-28 17:02:22 +0400

    Time: 36 ms (77.98%), Space: 14.6 MB (95.85%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -4,15 +4,7 @@ class Solution:
         翻转一句话中的每一个单词，同时句子还保持原来的顺序
         
         '''
-        # def reverse(word, i,j):
-        #     while(i<j):
-        #         tmp = word[i]
-        #         word[i] = word[j]
-        #         word[j] = tmp
-        #         i+=1
-        #         j-=1
-        res = []        
-        for word in s.split(" "):
-            res.append(word[::-1])
-        return " ".join(res)
+
+        
+        return " ".join(s.split(" ")[::-1])[::-1]
         
