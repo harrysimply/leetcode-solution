72ca964ab244201e8e1664867751eaf4276f1fb0
Huang Xu <837851874@qq.com>
2022-01-28 15:51:27 +0400

    Time: 212 ms (48.52%), Space: 15.5 MB (17.24%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         使用双指针
-        使用一个指针来记录结果非0的位数
+        使用两个指针来记录0值和非0值，explorer指针用来探索非0位，anchor指针表示结果数组，如果有非0位，则互换explorer和anchor位的值。
         """
         anchor = 0
         
