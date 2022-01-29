b939c9ca7df1effc7627b9dd7396013c8208083d
Huang Xu <837851874@qq.com>
2022-01-29 09:56:00 +0400

    Time: 1160 ms (31.73%), Space: 31.1 MB (7.69%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -4,15 +4,19 @@ class Solution:
         柱状图的最大面积，和之前的围成最大面积区域问题有点像
         先遍历一遍所有柱状图的面积，找出最大面积的柱子然后把结果给res
         之后最大面积的矩形都是近邻柱子的面积
+        参考：https://www.youtube.com/watch?v=zx5Sw9130L0
         '''
         maxArea =0
         stack = [] #pair: (index, height)
         for i, h in enumerate(heights):
             start = i
+            # 下一个柱子的高度比栈顶height小时，出栈
             while stack and stack[-1][1] > h:
                 index, height = stack.pop()
+                # 使用heigt*(i-index)计算当前最大面积
                 maxArea = max(maxArea, height*(i-index))
                 start = index
+            # 将刷新的start和高度压入栈    
             stack.append((start,h))
         for i,h in stack:
             maxArea = max(maxArea, h*(len(heights)-i))
