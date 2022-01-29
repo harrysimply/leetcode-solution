0a6f7ace23411c74af802c5c798461e31d302f88
Huang Xu <837851874@qq.com>
2022-01-29 11:07:50 +0400

    Time: 36 ms (68.17%), Space: 13.8 MB (99.98%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -7,20 +7,22 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         '''
         找到倒数第n个链表节点，然后删掉它。
-        参考官方节点，可以使用快慢指针法。
+        参考官方题解可以使用快慢指针法。
         '''
         fast, slow =  head, head
         
+        # 先让快指针移动n步
         for _ in range(n):
-            # 让快指针移动n步
             fast = fast.next
-        # 如果快指针已经到头，则证明要n是链表长度，因此需要删除第一个链表节点，直接返回第二个链表
+        # 如果快指针移动n步就到链表的结尾了，表示删除的就是第一位
         if not fast:
             return head.next
-        # 当fast.next不为空时，移动快慢指针，直到fast.next为None
+        
+        # 当fast还没到结尾时循环不要\U0001f442
         while fast.next:
-            slow, fast = slow.next, fast.next
-        # 此时，删掉slow慢指针的后一个链表节点
+            fast = fast.next
+            slow = slow.next
+         
         slow.next = slow.next.next
         return head
             
