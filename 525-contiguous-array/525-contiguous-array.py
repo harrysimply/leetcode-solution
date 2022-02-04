7ea43672d959a9d64ef65b7f1451cedda2a8033d
Huang Xu <837851874@qq.com>
2022-02-04 11:44:38 +0400

    Time: 1034 ms (48.19%), Space: 19.5 MB (13.61%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -4,9 +4,13 @@ class Solution:
         给一个0和1组成的数组，返回最有可能的连续子数组，其中子数组拥有相同数量的0和1
         读题后觉得这个题目和双指针以及HashTable很接近
         
+        画图理解会相对容易些
+        
         用一个map去存count的和和当前index，当遇到0时，count-1，当遇到1时，count+1
         并把当前count和index保存在map中
         当count值再次出现时，两个index之差就是最长长度，maxLength = max(maxLenth, index-hashTable(count))
+        
+        
         
         '''
         hashTable = {0:-1}
