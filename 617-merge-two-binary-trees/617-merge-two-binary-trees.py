b68bed3176606527bfe48eae92535da2d822e36b
Huang Xu <837851874@qq.com>
2022-02-05 02:07:13 +0400

    Time: 101 ms (53.60%), Space: 15.5 MB (59.87%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        合并两棵二叉树
+        使用回溯算法，树的左边+左边，右边+右边
+        '''
+        if root1 and root2: 
+            ans = TreeNode(root1.val+ root2.val)
+            ans.left = self.mergeTrees(root1.left, root2.left)
+            ans.right = self.mergeTrees(root1.right, root2.right)
+            return ans
+        else:
+            return root1 or root2 
+        
+        
