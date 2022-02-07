6da1d2aa8e81c162afecb5080ab1bb8e256fe084
Huang Xu <837851874@qq.com>
2022-02-08 00:43:23 +0400

    Time: 502 ms (54.91%), Space: 15.9 MB (22.18%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -3,8 +3,6 @@ class Solution:
         '''
         给两个整数n和k，返回所有可能的[1,n]集合中k长度的组合
         边界条件： 1<n<=20, 1<k <=n
-        
-        '''
         def dfs(nums, k , index, path, res):
             if k == 0:
                 res.append(path)
@@ -14,6 +12,19 @@ class Solution:
         res = []
         dfs(range(1,n+1),k,0,[], res)
         return res
+        '''
+        res = []
+        
+        def backtrack(start, comb):
+            if len(comb)==k:
+                res.append(comb.copy())
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1, comb)
+                comb.pop()
+        backtrack(1, [])
+        return res
+                
 
         
         
