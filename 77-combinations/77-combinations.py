531aa8ab3558923b58dbd5e5140c8ce687123108
Huang Xu <837851874@qq.com>
2022-02-07 23:01:42 +0400

    Time: 576 ms (40.65%), Space: 15.9 MB (32.74%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        '''
+        给两个整数n和k，返回所有可能的[1,n]集合中k长度的组合
+        边界条件： 1<n<=20, 1<k <=n
+        
+        '''
+        def dfs(nums, k , index, path, res):
+            if k == 0:
+                res.append(path)
+
+            for i in range(index, len(nums)):
+                dfs(nums, k-1, i+1, path+[nums[i]], res)
+        res = []
+        dfs(range(1,n+1),k,0,[], res)
+        return res
+
+        
+        
